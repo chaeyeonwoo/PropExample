a9337aa0c2cbbb291b24eecf67d3185a638bd3c1
chaeyeonwoo <codusdn0210@gmail.com>
2025-05-27 14:51:15 +0900

    Box 클래스 게터세터 구현

--- Box.cs
+++ Box.cs
@@ -24,6 +24,22 @@ namespace PropExample
                 // throw new Exception("너비와 높이는 자연수로 초기화 해주세요.");
             }
         }
+
+        public int getWidth() { return this.width; }
+        public int getHeight() { return this.height; }
+
+        public void setWidth(int width)
+        {
+            if (width > 0) { this.width = width; }
+            else { Console.WriteLine("너비는 자연수로 입력해주세요."); }
+
+        }
+
+        public void setHeight(int height)
+        {
+            if (height > 0) { this.height = height; }
+            else { Console.WriteLine("높이는 자연수로 입력해주세요."); }
+        }
         public int Area()
         {
             return this.width * this.height;
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PropExample
         static void Main(string[] args)
         {
             Box box1 = new Box(100, 200);
-            Console.WriteLine(box1);
+            Console.WriteLine(box1.Area());
         }
     }
 }
